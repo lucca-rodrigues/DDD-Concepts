3f77fd2fb1902e9092458bb8ba37201021042a43
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-06-07 17:26:06 -0300

    implement order tests

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -1,5 +1,6 @@
 import { Address } from "./address";
 import { Order } from "./order";
+import { OrderItem } from "./orderItem";
 
 describe("Customer unit tests", () => {
   it("should throw when id is empty ", () => {
@@ -18,5 +19,21 @@ describe("Customer unit tests", () => {
     expect(() => {
       let order = new Order("123", "123", []);
     }).toThrowError("Items are required from order");
+  });
+
+  it("should calculate total", () => {
+    const item = new OrderItem("i1", "Item1", 100);
+    const itemTwo = new OrderItem("i1", "Item1", 150);
+
+    const order = new Order("123", "123", [item]);
+    const orderTwo = new Order("123", "123", [item, itemTwo]);
+    const orderTotal = order.total();
+    const orderTwoTotal = order.total();
+
+    const total = order.total();
+    const orderTotalTotal = orderTwo.total();
+
+    expect(total).toBe(100);
+    expect(orderTotalTotal).toBe(250);
   });
 });
