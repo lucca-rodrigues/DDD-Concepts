e5d085ab7ac25766f555a1737226f56a51e6569d
Lucas <lucas@MacBook-Air-de-Lucas.local>
2022-06-10 20:35:00 -0300

    feat: implement rewardPoints test from customer

--- src/service/product/product.service.ts
+++ src/service/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Product } from "entity/product";
+import { Product } from "../../entity/product";
 
 export class ProductService {
   static incriasePrice(products: Product[], percentage: number): Product[] {
